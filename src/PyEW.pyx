ac0e8da4c3cf4c2a4b0d82ae7440121253a9c597
jasondec <jdecristofaro@usgs.gov>
2026-08-04 23:11:04 -0700

    fix dict attriute

--- src/PyEW.pyx
+++ src/PyEW.pyx
@@ -415,8 +415,8 @@ cdef class EWModule:
         'startt': mypkt.trh2.starttime,
         'endt': mypkt.trh2.endtime,
         'datatype': mypkt.trh2.datatype.decode('UTF-8'),
-        'modid': msg[3].mod,
-        'instid': msg[3].instid,
+        'instid': msg[3]['instid'],
+        'modid': msg[3]['mod'],
         'data': myarr}
 
         if datatype == 's4':
@@ -430,8 +430,8 @@ cdef class EWModule:
           'startt': struct.unpack("<d", struct.pack(">d", mypkt.trh2.starttime))[0],
           'endt': struct.unpack("<d", struct.pack(">d", mypkt.trh2.endtime))[0],
           'datatype': mypkt.trh2.datatype.decode('UTF-8'),
-          'modid': msg[3].mod,
-          'instid': msg[3].instid,
+          'instid': msg[3]['instid'],
+          'modid': msg[3]['mod'],
           'data': myarr}
 
         return data
